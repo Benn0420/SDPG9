d6e5ec813a7ef7414fdee17d4147e23ea8a3181e
Benn0420 <benn0420@algonquinlive.com>
2024-03-29 10:04:21 -0400

    Sprint 3:
    - Tableau descending enforced
    - Stock drawing to Talon
    - Talon shuffling to bottom of Stock
    - Points & Timer label
    - +5 points for foundation moves
    - timer starts after first click

--- cardgame.py
+++ cardgame.py
@@ -10,6 +10,7 @@ from constants import *
 
 class SolitaireGame(arcade.Window):
     """Main application class."""
+
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
@@ -29,8 +30,26 @@ class SolitaireGame(arcade.Window):
         # piles
         self.piles = None
 
+        # scoring
+        self.points = -52
+        self.points_text = None
+
+        # timing
+        self.game_underway = False
+        self.total_time = 0.0
+        self.timer_text = None
+
     def setup(self):
         """Set up the game"""
+
+        # text object for point scoring
+        self.points_text = arcade.Text(f"Points: {self.points}", POINTS_X, POINTS_Y,
+                                       arcade.csscolor.LIGHT_GREEN, 40, bold=True)
+
+        # text object for timer
+        self.timer_text = arcade.Text("Time: 00:00", TIMER_X, TIMER_Y,
+                                      arcade.csscolor.LIGHT_GREEN, 40, bold=True)
+
         # handle held cards:
         self.held_cards = []
 
@@ -108,8 +127,16 @@ class SolitaireGame(arcade.Window):
         # draw cards
         self.card_list.draw()
 
+        # draw points
+        self.points_text.draw()
+
+        # draw timer
+        self.timer_text.draw()
+
     def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
         """for mouse press"""
+
+        self.game_underway = True
 
         # get list of cards clicked
         cards = arcade.get_sprites_at_point((x, y), self.card_list)
@@ -118,27 +145,47 @@ class SolitaireGame(arcade.Window):
         if len(cards) > 0:
             # top of stack only
             primary_card = cards[-1]
-
             # what pile is the card in?
             pile_index = self.get_pile_of_card(primary_card)
 
-            if primary_card.is_face_down:
-                primary_card.face_up()
-            else:
-                # get card being clicked
-                self.held_cards = [primary_card]
-                # set position
-                self.held_cards_original_location = [self.held_cards[0].position]
-                # put it on top
-                self.pull_to_top(self.held_cards[0])
+            # if it's the stockpile
+            if pile_index == STOCK:
+                if primary_card.is_face_down:
+                    # flip the card face up
+                    primary_card.face_up()
 
-                # handle stack of cards
-                card_index = self.piles[pile_index].index(primary_card)
-                for i in range(card_index + 1, len(self.piles[pile_index])):
-                    card = self.piles[pile_index][i]
-                    self.held_cards.append(card)
-                    self.held_cards_original_location.append(card.position)
-                    self.pull_to_top(card)
+                # see if there is a card in the talon pile
+                if self.piles[TALON]:
+                    # card in talon pile goes to the bottom of the stock
+                    card_in_talon = self.piles[TALON].pop(0)
+                    card_in_talon.face_down()
+                    self.move_card_to_pile(card_in_talon, 0)
+                    self.push_to_back(card_in_talon)
+                    card_in_talon.position = self.pile_mat_list[STOCK].position
+                # move the card to the talon pile
+                primary_card.position = self.pile_mat_list[TALON].position
+                self.move_card_to_pile(primary_card, TALON)
+                # LOG: Print the number of cards in the stockpile after each click
+                print("Number of cards in stockpile:", len(self.piles[STOCK]))
+            # if it's any other pile
+            else:
+                if primary_card.is_face_down:
+                    primary_card.face_up()
+                else:
+                    # get card being clicked
+                    self.held_cards = [primary_card]
+                    # set position
+                    self.held_cards_original_location = [self.held_cards[0].position]
+                    # put it on top
+                    self.pull_to_top(self.held_cards[0])
+
+                    # handle stack of cards
+                    card_index = self.piles[pile_index].index(primary_card)
+                    for i in range(card_index + 1, len(self.piles[pile_index])):
+                        card = self.piles[pile_index][i]
+                        self.held_cards.append(card)
+                        self.held_cards_original_location.append(card.position)
+                        self.pull_to_top(card)
 
     def on_mouse_release(self, x: float, y: float, button: int, modifiers: int):
         """for mouse release"""
@@ -165,11 +212,10 @@ class SolitaireGame(arcade.Window):
             elif TABLEAU_1 <= pile_index <= TABLEAU_7:
                 # check if empty
                 if len(self.piles[pile_index]) == 0:
-                    # no cards in tableau pile
+                    # check if it's a King
                     if self.held_cards[0].value == "K":
-                        print("no cards in pile")
+                        # Move cards to proper position
                         for i, dropped_card in enumerate(self.held_cards):
-                            # Move cards to proper position
                             dropped_card.position = pile.center_x, \
                                 pile.center_y - CARD_VERTICAL_OFFSET * i
 
@@ -183,17 +229,22 @@ class SolitaireGame(arcade.Window):
                 else:
                     # pile is not empty, read top card
                     top_card = self.piles[pile_index][-1]
-                    # check for alternating colors
-                    if self.is_alternating_color(top_card, self.held_cards[0]):
-                        for i, dropped_card in enumerate(self.held_cards):
-                            dropped_card.position = top_card.center_x, \
-                                top_card.center_y - CARD_VERTICAL_OFFSET * (i + 1)
-                        # assign card to correct pile list
-                        for card in self.held_cards:
-                            self.move_card_to_pile(card, pile_index)
-                        reset_position = False
+                    print("held card value" + str(CARD_VALUES.index(self.held_cards[0].value)))
+                    print("top card value" + str(CARD_VALUES.index(top_card.value)))
+                    if CARD_VALUES.index(self.held_cards[0].value) - CARD_VALUES.index(top_card.value) == -1:
+                        # check for alternating colors
+                        if self.is_alternating_color(top_card, self.held_cards[0]):
+                            for i, dropped_card in enumerate(self.held_cards):
+                                dropped_card.position = top_card.center_x, \
+                                    top_card.center_y - CARD_VERTICAL_OFFSET * (i + 1)
+                            # assign card to correct pile list
+                            for card in self.held_cards:
+                                self.move_card_to_pile(card, pile_index)
+                            reset_position = False
+                        else:
+                            print("Invalid move: Cards must be alternating in color")
                     else:
-                        print("Invalid move: Cards must be alternating in color")
+                        print("Invalid move: Cards must be descending in order")
 
             # if trying to add to foundation, only one card can be added at a time
             elif FOUNDATION_1 <= pile_index <= FOUNDATION_4 and len(self.held_cards) == 1:
@@ -202,6 +253,7 @@ class SolitaireGame(arcade.Window):
                     if self.held_cards[0].value == "A":
                         self.held_cards[0].position = pile.position
                         self.move_card_to_pile(self.held_cards[0], pile_index)
+                        self.increase_points(5)
                         reset_position = False
                     else:
                         print("Can only start foundation with Ace")
@@ -212,6 +264,7 @@ class SolitaireGame(arcade.Window):
                         if CARD_VALUES.index(self.held_cards[0].value) - CARD_VALUES.index(top_card.value) == 1:
                             self.held_cards[0].position = pile.position
                             self.move_card_to_pile(self.held_cards[0], pile_index)
+                            self.increase_points(5)
                             reset_position = False
                     else:
                         print("Invalid move for foundation pile")
@@ -239,6 +292,13 @@ class SolitaireGame(arcade.Window):
         self.card_list.remove(card)
         self.card_list.append(card)
 
+    def push_to_back(self, card: arcade.Sprite):
+        """Push the cards to the bottom of the rendering order"""
+
+        # put the card you are selecting to the bottom of rendering
+        self.card_list.remove(card)
+        self.card_list.insert(0, card)
+
     def get_pile_of_card(self, card):
         for index, pile in enumerate(self.piles):
             if card in pile:
@@ -265,6 +325,18 @@ class SolitaireGame(arcade.Window):
             return True
         else:
             return False
+
+    def on_update(self, delta_time):
+        if self.game_underway:
+            self.total_time += delta_time
+            # update timer text
+            minutes = int(self.total_time) // 60
+            seconds = int(self.total_time) % 60
+            self.timer_text.text = f"Time: {minutes:02d}:{seconds:02d}"
+
+    def increase_points(self, amount):
+        self.points += amount
+        self.points_text.text = f"Points: {self.points}"
 
 
 def main():
